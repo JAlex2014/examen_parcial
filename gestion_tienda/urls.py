e80b575ae7453c4af6f90fca72213d12f3031198
JAlex2014 <agomezf@pucp.edu.pe>
2023-04-08 09:04:23 -0500

    funcionalidad gestion usuarios 90%

--- gestion_tienda/urls.py
+++ gestion_tienda/urls.py
@@ -5,5 +5,8 @@ app_name = 'gestion_tienda'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('consolaUsuario', views.consolaUsuario, name = 'consolaUsuario')
+    path('consolaUsuario', views.consolaUsuario, name = 'consolaUsuario'),
+    path('cerrarSesion', views.cerrarSesion, name='cerrarSesion'),
+    path('gestionUsuarios', views.gestionUsuarios, name='gestionUsuarios'),
+    path('eliminarUsuario/<str:ind>', views.eliminarUsuario, name='eliminarUsuario')
 ]
--- gestion_tienda/views.py
+++ gestion_tienda/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Usuario
 
 # Create your views here.
 def index(request):
@@ -21,3 +23,42 @@ def index(request):
 @login_required(login_url = 'http://127.0.0.1:8000')
 def consolaUsuario(request):
     return render(request,'consolaUsuario.html')
+
+@login_required(login_url = 'http://127.0.0.1:8000')
+def gestionUsuarios(request):
+    if request.method == 'POST':
+        usernameUsuario = request.POST.get('usernameUsuario')
+        contraUsuario = request.POST.get('contraUsuario')
+        nombreUsuario = request.POST.get('nombreUsuario')
+        apellidoUsuario = request.POST.get('apellidoUsuario')
+        rolUsuario = request.POST.get('rolUsuario')
+        emailUsuario = request.POST.get('emailUsuario')
+        celUsuario = request.POST.get('celUsuario')
+        usuarioNuevo = User.objects.create(
+            username = usernameUsuario,
+            email = emailUsuario)
+        usuarioNuevo.set_password(contraUsuario)
+        usuarioNuevo.first_name = nombreUsuario
+        usuarioNuevo.last_name = apellidoUsuario
+        usuarioNuevo.is_staff = True
+        usuarioNuevo.save()
+        Usuario.objects.create(
+            user = usuarioNuevo,
+            Rol_del_usuario = rolUsuario,
+            Nro_de_celular = celUsuario,
+        )
+        return HttpResponseRedirect(reverse('gestion_tienda:gestionUsuarios'))
+    return render(request,'gestionUsuarios.html', {
+        'usuariosTotales': User.objects.all().order_by('id')
+    })
+
+def eliminarUsuario(request,ind):
+    usuarioEliminar = User.objects.get(id=ind)
+    Usuario.objects.get(user = usuarioEliminar).delete()
+    usuarioEliminar.delete()
+    return HttpResponseRedirect(reverse('gestion_tienda:gestionUsuarios'))
+    
+@login_required(login_url = 'http://127.0.0.1:8000')
+def cerrarSesion(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('gestion_tienda:index'))
